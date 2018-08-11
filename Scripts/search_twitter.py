ce75002ff8a51c226045e2c70718906ea8386a19
FranziSchmitt <franziska.schmitt87@gmail.com>
2018-08-11 15:35:51 +0200

    adjusted search

--- Scripts/search_twitter.py
+++ Scripts/search_twitter.py
@@ -112,12 +112,13 @@ def main():
                      'LTWBY', 'LTWBY18',
                      'Gruene','LINKE', 
                      'FDP', 'Grüne',
-                     'bayern', 'baden-wuerttenberg',
-                     'hessen', 'nrw', 'nordrhein-westfalen',
-                     'sachsen', 'sachsen-anhalt',
-                     'niedersachsen', 'mecklemburg-vorpommern',
-                     'thueringen', 'brandenburg', 'berlin', 'hamburg', 
-                     'bremen', 'rheinland-pfalz', 'saarland']
+                     #'bayern', 'baden-wuerttenberg',
+                     #'hessen', 'nrw', 'nordrhein-westfalen',
+                     #'sachsen', 'sachsen-anhalt',
+                     #'niedersachsen', 'mecklemburg-vorpommern',
+                     #'thueringen', 'brandenburg', 'berlin', 'hamburg', 
+                     #'bremen', 'rheinland-pfalz', 'saarland'
+                    ]
     
     time_limit = 5                           # runtime limit in hours
     max_tweets = 100                           # number of tweets per search (will be
@@ -131,83 +132,88 @@ def main():
 
     # loop over search items,
     # creating a new file for each
-    for search_phrase in search_phrases:
+    last_day = 10
+    
+    for max_days_old in range(last_day, 1, -1):
+        min_days_old = max_days_old - 1 
+            
+        for search_phrase in search_phrases:
 
-        print('Search phrase =', search_phrase)
+            print('Search phrase =', search_phrase)
 
-        ''' other variables '''
-        name = search_phrase.split()[0]
-        dir_base = os.path.dirname(os.path.realpath(__file__))
-        dir_save = os.path.join(dir_base, '../Data')
-        json_file_root = os.path.join(dir_save,  name)
-        
-        os.makedirs(json_file_root, exist_ok=True)
-        read_IDs = False
-        
-        # open a file in which to store the tweets
-        # if scraped time span is exactly one day
-        if max_days_old - min_days_old == 1:
-            d = dt.datetime.now() - dt.timedelta(days=min_days_old)
-            day = '{0}-{1:0>2}-{2:0>2}'.format(d.year, d.month, d.day)
-        else:
-            d1 = dt.datetime.now() - dt.timedelta(days=max_days_old-1)
-            d2 = dt.datetime.now() - dt.timedelta(days=min_days_old)
-            day = '{0}-{1:0>2}-{2:0>2}_to_{3}-{4:0>2}-{5:0>2}'.format(
-                  d1.year, d1.month, d1.day, d2.year, d2.month, d2.day)
-        file_name = name + '_' + day + '.json'
-        json_file = os.path.join(json_file_root, file_name)
-        # check if file for search term already exists
-        if os.path.isfile(json_file):
-            print('Appending tweets to file named: ',json_file)
-            # flag for continue
-            read_IDs = True
-        
-        # authorize and load the twitter API
-        api = load_api()
-        
-        # set the 'starting point' ID for tweet collection
-        if read_IDs:
-            # open the json file and get the latest tweet ID
-            with open(json_file, 'r') as f:
-                lines = f.readlines()
-                max_id = json.loads(lines[-1])['id']
-                print('Searching from the bottom ID in file')
-        else:
-            # get the ID of a tweet that is min_days_old
-            if min_days_old == 0:
-                max_id = -1
+            ''' other variables '''
+            name = search_phrase.split()[0]
+            dir_base = os.path.dirname(os.path.realpath(__file__))
+            dir_save = os.path.join(dir_base, '../Data')
+            json_file_root = os.path.join(dir_save,  name)
+            
+            os.makedirs(json_file_root, exist_ok=True)
+            read_IDs = False
+            
+            # open a file in which to store the tweets
+            # if scraped time span is exactly one day
+            if max_days_old - min_days_old == 1:
+                d = dt.datetime.now() - dt.timedelta(days=min_days_old)
+                day = '{0}-{1:0>2}-{2:0>2}'.format(d.year, d.month, d.day)
             else:
-                max_id = get_tweet_id(api, days_ago=(min_days_old-1))
-        # set the smallest ID to search for
-        since_id = get_tweet_id(api, days_ago=(max_days_old-1))
-        print('max id (starting point) =', max_id)
-        print('since id (ending point) =', since_id)
-
-        ''' tweet gathering loop  '''
-        start = dt.datetime.now()
-        end = start + dt.timedelta(hours=time_limit)
-        count, exitcount = 0, 0
-        while dt.datetime.now() < end:
-            # collect tweets and update max_id
-            tweets, max_id, internal_counter = tweet_search(api, search_phrase, max_tweets,
-                                                            max_id=max_id, since_id=since_id
-                                                            )
-            count += internal_counter
-            print('count =',count)
-            # write tweets to file in JSON format
-            if tweets:
-                write_tweets(tweets, json_file)
-                exitcount = 0
+                d1 = dt.datetime.now() - dt.timedelta(days=max_days_old-1)
+                d2 = dt.datetime.now() - dt.timedelta(days=min_days_old)
+                day = '{0}-{1:0>2}-{2:0>2}_to_{3}-{4:0>2}-{5:0>2}'.format(
+                    d1.year, d1.month, d1.day, d2.year, d2.month, d2.day)
+            file_name = name + '_' + day + '.json'
+            json_file = os.path.join(json_file_root, file_name)
+            # check if file for search term already exists
+            if os.path.isfile(json_file):
+                print('Appending tweets to file named: ',json_file)
+                # flag for continue
+                read_IDs = True
+            
+            # authorize and load the twitter API
+            api = load_api()
+            
+            # set the 'starting point' ID for tweet collection
+            if read_IDs:
+                # open the json file and get the latest tweet ID
+                with open(json_file, 'r') as f:
+                    lines = f.readlines()
+                    max_id = json.loads(lines[-1])['id']
+                    print('Searching from the bottom ID in file')
             else:
-                exitcount += 1
-                if exitcount == 3:
-                    if search_phrase == search_phrases[-1]:
-                        sys.exit('Maximum number of empty tweet strings reached - exiting')
-                    else:
-                        print('Maximum number of empty tweet strings reached - breaking')
-                        break
-        if dt.datetime.now() < end:
-            print('I ran out of time, please check: time_limit')
+                # get the ID of a tweet that is min_days_old
+                if min_days_old == 0:
+                    max_id = -1
+                else:
+                    max_id = get_tweet_id(api, days_ago=(min_days_old-1))
+            # set the smallest ID to search for
+            since_id = get_tweet_id(api, days_ago=(max_days_old-1))
+            print('max id (starting point) =', max_id)
+            print('since id (ending point) =', since_id)
+
+            ''' tweet gathering loop  '''
+            start = dt.datetime.now()
+            end = start + dt.timedelta(hours=time_limit)
+            count, exitcount = 0, 0
+            while dt.datetime.now() < end:
+                # collect tweets and update max_id
+                tweets, max_id, internal_counter = tweet_search(api, search_phrase, max_tweets,
+                                                                max_id=max_id, since_id=since_id
+                                                                )
+                count += internal_counter
+                print('count =',count)
+                # write tweets to file in JSON format
+                if tweets:
+                    write_tweets(tweets, json_file)
+                    exitcount = 0
+                else:
+                    exitcount += 1
+                    if exitcount == 3:
+                        if search_phrase == search_phrases[-1]:
+                            sys.exit('Maximum number of empty tweet strings reached - exiting')
+                        else:
+                            print('Maximum number of empty tweet strings reached - breaking')
+                            break
+            if dt.datetime.now() < end:
+                print('I ran out of time, please check: time_limit')
 
 
 if __name__ == "__main__":
